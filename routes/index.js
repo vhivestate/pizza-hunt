cab8521fda547ec9def9c8a5e73faa10982c7cb5
vhivestate <garcia.valeria001@gmail.com>
2022-07-23 15:12:53 -0600

    test routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
+const pizzaRoutes = require('./api')
 
 router.use('/', htmlRoutes);
+router.use('/api', pizzaRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
